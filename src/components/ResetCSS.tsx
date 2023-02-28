6e1c211cb2e5788fa8469fcad894dbe6a9816d88
wherehows <sensitive.hu@gmail.com>
2023-02-28 16:43:12 +0900

    chore: ResetCSS 컴포넌트 작성

--- src/components/ResetCSS.tsx
+++ src/components/ResetCSS.tsx
@@ -1,8 +1,59 @@
-import { Global } from '@emotion/react';
+import { Global, css } from '@emotion/react';
 import React from 'react';
 
 const ResetCSS = () => {
-  return <Global styles={css``} />;
+  return (
+    <Global
+      styles={css`
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
+
+        html,
+        body {
+          height: 100%;
+          font-family: 'Noto Sans KR', sans-serif;
+        }
+
+        body {
+          line-height: 1.5;
+          -webkit-font-smoothing: antialiased;
+        }
+
+        img,
+        picture,
+        video,
+        canvas,
+        svg {
+          display: block;
+          max-width: 100%;
+        }
+
+        input,
+        button,
+        textarea,
+        select {
+          font: inherit;
+        }
+
+        p,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          overflow-wrap: break-word;
+        }
+
+        #___gatsby {
+          isolation: isolate;
+        }
+      `}
+    />
+  );
 };
 
 export default ResetCSS;
