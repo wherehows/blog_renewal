5d49c1fda7139ceebfc39b82d66d213987df7b55
wherehows <sensitive.hu@gmail.com>
2023-03-01 22:49:28 +0900

    chore: 다크 모드 관련 함수 작성

---
+++ src/utils/darkMode.ts
@@ -0,0 +1,39 @@
+const isTheme = (theme: string): theme is Theme =>
+  theme === 'dark' || theme === 'light'
+
+function setTheme(theme: Theme) {
+  window.__theme = theme
+
+  if (theme === 'dark') {
+    document.body.className = 'dark'
+  } else {
+    document.body.className = 'light'
+  }
+}
+
+window.__setPreferredTheme = function (theme) {
+  setTheme(theme)
+
+  try {
+    localStorage.setItem('preferred-theme', theme)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+let preferredTheme: Theme | null = null
+
+try {
+  const storedTheme = localStorage.getItem('preferred-theme')
+
+  if (storedTheme && isTheme(storedTheme)) {
+    preferredTheme = storedTheme
+  }
+} catch (e) {
+  console.error(e)
+}
+
+let darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'))
+
