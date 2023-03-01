c184942031d7fe2bc89e4a54de308f6ec0b0b142
wherehows <sensitive.hu@gmail.com>
2023-03-01 22:51:01 +0900

    feat: gatsby-ssr에 다크 모드 관련 함수 set

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,8 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([<script src="./src/utils/darkMode.ts" />])
+}
+
